20b611a076eb139881dcb131ceb0315e026c1a8e
Darth_neket <nikitacherenkov1810@gmail.com>
2024-02-27 14:47:56 +0300

    add design for the login_page

--- app/src/main/java/com/example/films_mob_app/MainActivity.kt
+++ app/src/main/java/com/example/films_mob_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.films_mob_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
